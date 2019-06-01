7e72680322f2ea07cd0c35864da761c279d507f3
edwards33 <err@inbox.lv>
2019-06-02 00:18:39 +0300

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
+import Column from './column'
+
 const App = () => 'Hello World'
 
 render(<App />, document.getElementById('root'));
