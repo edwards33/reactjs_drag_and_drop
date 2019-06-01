e20390dee37997ad60e28d15af61c09375081dd8
edwards33 <err@inbox.lv>
2019-06-02 00:24:33 +0300

    Column component added

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-
+import initData from './data';
 import Column from './column'
 
-const App = () => 'Hello World'
+class App extends React.Component {
+  state = initData;
+
+  render() {
+    return this.state.columnOrder.map(columnID => {
+      const column = this.state.columns[columnID];
+      const tasks = column.taskIds.map(taskID => this.state.tasks[taskID]);
+
+      return <Column key={column.id} column={column} tasks={tasks} />;
+    });
+  }
+}
 
 render(<App />, document.getElementById('root'));
