a37388b1cdb8d5ef2aac5678d87a81e10fe7ebc4
edwards33 <err@inbox.lv>
2019-06-01 23:33:49 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-z1hk85
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-z1hk85)
---
+++ data.js
@@ -0,0 +1,7 @@
+const initData = {
+  task: {
+    'task01': {id: 'task01', content: '1. Task'},
+    'task02': {id: 'task02', content: '2. Task'},
+    'task03': {id: 'task03', content: '3. Task'},
+  }
+}
